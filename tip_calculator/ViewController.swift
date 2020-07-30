8852c7307a0edaa466d1f62edc68beef255c8f42
RadhyyahHossainb <radiabxd@gmail.com>
2020-07-29 20:28:48 -0400

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by Abdullah Ridwan on 7/29/20.
+//  Copyright © 2020 Abdullah Ridwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
