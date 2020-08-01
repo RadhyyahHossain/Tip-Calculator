acc71c2fcaa5fc7d52be42aff113cdc7889c6ec7
RadhyyahHossainb <radiabxd@gmail.com>
2020-08-01 16:34:35 -0400

    changes so everything basic works and added the split labels

--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func OnTap(_ sender: Any) {
+    print("Hello")
+    view.endEditing(true)
+    }
 
+    @IBAction func CalculateTip(_ sender: Any) {
+    
+        //Get the bill amount
+        let billAmount = Double(BillField.text!) ?? 0
+    
+        
+        //Calculate tip and total
+        let tipPercentages = [0.05, 0.10, 0.15, 0.20, 0.25]
+        
+        let tip = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = billAmount + tip
+        
+        //Update tip and total labels
+        TipLabel.text = String(format: "$%.2f", tip)
+        TotalLabel.text = String(format: "$%.2f", total)
+    
+    }
+    
+    
+    @IBAction func calculateSplit(_ sender: Any) {
+        
+        print("Hi")
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
 }
 
